886d0fdc976b7ee6749fffb9104ef02c94602e64
chipmunk-dev <devquokkajeong@gmail.com>
2023-01-03 15:47:47 +0900

    feat: AppConfigurer 적용 -> Container 역활

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,9 +3,18 @@ package app;
 import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args){
 
-        OrderApp orderApp = new OrderApp();
+        AppConfigurer appConfigurer = new AppConfigurer();
+
+        OrderApp orderApp = new OrderApp(
+                appConfigurer.productRepository(),
+                appConfigurer.menu(),
+                appConfigurer.cart(),
+                appConfigurer.order()
+        );
+
         orderApp.start();
     }
 }
--- src/app/OrderApp.java
+++ src/app/OrderApp.java
@@ -13,17 +13,21 @@ import java.util.Scanner;
 
 public class OrderApp {
 
+    private ProductRepository productRepository;
+    private Menu menu;
+    private Cart cart;
+    private Order order;
+
+    public OrderApp(ProductRepository productRepository, Menu menu, Cart cart, Order order) {
+        this.productRepository = productRepository;
+        this.menu = menu;
+        this.cart = cart;
+        this.order = order;
+    }
+
     public void start(){
 
         Scanner scanner = new Scanner(System.in);
-        ProductRepository productRepository = new ProductRepository();
-        Product[] products = productRepository.getAllProducts();
-        Menu menu = new Menu(products);
-        Cart cart = new Cart(productRepository, menu);
-        Order order = new Order(cart, new Discount(new DiscountCondition[] {
-                new CozDiscountCondition(new FixedRateDiscountPolicy(10)),
-                new KidDiscountCondition(new FixedAmountDiscountPolicy(500))
-        }));
 
         System.out.println("🍟 BurgerQueen Order Service");
 
